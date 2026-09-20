af50fb634b87f7af673b65e9cb50795dcf2b644b
arsenal4eva <iamprofootballer@gmail.com>
2026-09-20 15:53:56 +0800

    added main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,28 @@
-const API_KEY =
+const API_KEY = import.meta.env.VITE_NASA_API_KEY;
+
+document.querySelector("#app").innerHTML = "<p>loading...</p>";
+
+fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
+	.then(response => response.json())
+	.then(data => {
+		let media;
+
+		if (data.media_type === "image") {
+			media = `<img src="${data.url}" style="width: 300px; height: 200px;">`
+		} else if (data.url.includes("youtube")) {
+			media = `<iframe src="${data.url}" width="600" height="400"></iframe>`
+		}else {
+			media = `<video src="${data.url}" controls></video>`
+		}
+		document.querySelector("#app").innerHTML = `
+	<h1>#{data.title}</h1>
+	${media}
+	<p>${data.explanation}</p>
+	`;
+	})
+	.catch(err => {
+		document.querySelector("#app").innerHTML = `<p>Error: ${err.message}</p>`;
+	});
+
+
+
